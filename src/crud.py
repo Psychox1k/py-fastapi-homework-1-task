6f71987fbdd0b3d9f22602e47742c115ce39cd8f
Psychox1k <success01001011@gmail.com>
2026-03-01 14:26:38 +0100

    Solution v1

---
+++ src/crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import func
+from database import models
+
+
+async def get_list_movies(db: AsyncSession, per_page: int, offset: int) -> list[models.MovieModel]:
+    stmt = select(models.MovieModel).offset(offset).limit(per_page)
+    result = await db.scalars(stmt)
+    return result.all()
+
+async def get_single_movie(db: AsyncSession, movie_id: int) -> models.MovieModel | None:
+    return await db.scalar(select(models.MovieModel).where(models.MovieModel.id == movie_id))
+
+async def get_movies_count(db: AsyncSession) -> int:
+    stmt = select(func.count()).select_from(models.MovieModel)
+    result = await db.scalar(stmt)
+    return result or 0
--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,47 @@
+import math
+
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 
+import crud
+import schemas
+from crud import get_single_movie
 from database import get_db, MovieModel
 
 
 router = APIRouter()
 
 # Write your code here
+@router.get("/movies/{movie_id}/", response_model=schemas.MovieDetailResponseSchema)
+async def get_movie(
+        movie_id: int,
+        db: Annotated[AsyncSession, Depends(get_db)]
+):
+    movie = await get_single_movie(db=db, movie_id=movie_id)
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
+
+@router.get("/movies/", response_model=schemas.MovieListResponseSchema)
+async def get_movies(
+        db: Annotated[AsyncSession, Depends(get_db)],
+        page: int = Query(default=1, ge=1),
+        per_page: int = Query(default=10, ge=1, le=20)
+):
+
+    movies = await crud.get_list_movies(db=db, per_page=per_page, offset=(page - 1) * per_page)
+    total_items = await crud.get_movies_count(db=db)
+    total_pages = math.ceil(total_items / per_page)
+
+    prev_page_url = f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page_url = f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page_url,
+        "next_page": next_page_url,
+        "total_pages": total_pages,
+        "total_items": total_items
+    }
